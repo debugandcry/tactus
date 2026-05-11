af7e46d9cd78664f0a12f03df78874f222e7f8a5
debugandcry <skandamaiyaaaaa@gmail.com>
2026-05-11 17:00:40 +0530

    committing version 0.0.2: implemented: -logging for debugging's sake, with state change information. -pause/play, restart, and exit by waiting for keypresses with `_getch()`. windows only for now. well it worked smooth, didnt work properly at first because i forgot to update the source code. ctrl + s saved me. well i did it again, and worked perfectly. except for latency, which is a separate conversation. about half a second late for each keypress, id say. yup the logs looked real legit. next im going to add in sample rate detection and seeking/skipping capabilities.

--- audio/audioengine.cpp
+++ audio/audioengine.cpp
@@ -4,16 +4,27 @@
 #include <string>
 #include <stdio.h>
 #include <iostream>
+#include <conio.h>
 std::string path = "C:/Users/grsma/mergere/audio/breathe_in_the_air.mp3";
 int main() {
+    bool is_running = true;
+    std::cout << "> program started, running... \n";
     ma_engine engine;
     ma_result status = ma_engine_init(NULL, &engine);
-    if (status != MA_SUCCESS) {std::cout << "can't setup engine \n" << status << "\n"; return -1;}
+    if (status != MA_SUCCESS) {std::cout << "> can't setup engine. \n" << status << "\n"; return -1;}
+    std::cout << "> engine initialized. \n";
     ma_sound song;
     ma_result song_status = ma_sound_init_from_file(&engine, path.c_str(), 0, NULL, NULL, &song);
-    if (song_status != MA_SUCCESS) {std::cout << "can't setup sound \n" << song_status << "\n"; return -1;}
+    if (song_status != MA_SUCCESS) {std::cout << "> can't setup sound. \n" << song_status << "\n"; return -1;}
+    std::cout << "> sound initialized. \n";
     ma_sound_start(&song);
-    std::cin.get();
-    ma_sound_uninit(&song);
-    ma_engine_uninit(&engine);
+    std::cout << "> sound started. \n";
+    while (is_running) {
+        char userinput = _getch();
+        if (userinput == ' ') {if (ma_sound_is_playing(&song) == MA_TRUE) {ma_sound_stop(&song); std::cout << "> space pressed, audio stopped. \n";} else {ma_sound_start(&song); std::cout << "> space pressed, audio playing \n";}}
+        else if (userinput == 'r') {ma_sound_stop(&song); ma_sound_seek_to_pcm_frame(&song, 0); ma_sound_start(&song); std::cout << "> 'r' pressed, audio restarted. \n";}
+        else if (userinput == 27) {ma_sound_stop(&song); is_running = false; std::cout << "> 'esc' pressed, exiting... \n";}
+    }
+    ma_sound_uninit(&song); ma_engine_uninit(&engine);
+    std::cout << "> cleanup successful, exiting... \n";
 }
