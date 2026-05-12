ee418acb5638dc0eac115b2c95636bf379394ce6
debugandcry <skandamaiyaaaaa@gmail.com>
2026-05-12 13:31:32 +0530

    committing version 0.0.4: so experience-wise, nothing much. just: -switched from blocking input collection to continuous polling. -added in an array of various helpers (2 or something). yup it worked well, noticeable improvements on the forward seek. adding in queuing and autoplay functionality next.

--- audio/audioengine.cpp
+++ audio/audioengine.cpp
@@ -1,14 +1,16 @@
 #define MA_ENABLE_AUDIO
 #define MINIAUDIO_IMPLEMENTATION
+#define WIN32_LEAN_AND_MEAN
 #include "miniaudio.h"
 #include <string>
 #include <stdio.h>
 #include <iostream>
-#include <conio.h>
 #include <fstream>
 #include <ctime>
 #include <chrono>
 #include <iomanip>
+#include <windows.h>
+#include <thread>
 
 std::ofstream logs;
 
@@ -20,6 +22,7 @@ struct Music {
     ma_uint32 sample_rate;
     ma_uint32 channels;
     ma_uint64 totalframes;
+    ma_uint64 skipframes;
     std::string name;
     std::string path;
     std::string artist;
@@ -38,6 +41,92 @@ void logger(const std::string& msg) {
         << std::setw(2) << std::setfill('0') << local_time->tm_sec << ":" 
         << " > " << msg << "\n";
     std::cout << "> " << msg << "\n";
+}
+
+struct KeyStates {
+    bool current;
+    bool previous;
+};
+
+KeyStates keys[256];
+
+bool pressed_query(int keyval) {
+    return keys[keyval].current && !keys[keyval].previous;
+}
+
+void update_query(int key) {
+    keys[key].previous = keys[key].current;
+    keys[key].current = GetAsyncKeyState(key) & 0x8000;
+} 
+
+void rest(int ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+
+void song_input_handler(Music &song) {
+    while (song_process_running) {
+        
+        update_query(VK_SPACE);
+        update_query(VK_ESCAPE);
+        update_query('R');
+        update_query(VK_RIGHT);
+        update_query(VK_LEFT);
+        
+        if (pressed_query(VK_ESCAPE)) {
+            ma_sound_stop(&song.sound);
+            song_process_running = false;
+            logger("'esc' key pressed, proceeding to exit.");
+        }
+        
+        else if (pressed_query(VK_SPACE)) {
+            if (ma_sound_is_playing(&song.sound) == MA_TRUE){
+                ma_sound_stop(&song.sound);
+                logger("spacebar pressed, audio playback paused.");    
+            }
+            else {
+                ma_sound_start(&song.sound);
+                logger("spacebar pressed, audio playback resumed.");
+            }
+        }
+
+        else if (pressed_query('R')) {
+            ma_sound_stop(&song.sound);
+            ma_sound_seek_to_pcm_frame(&song.sound, 0); 
+            ma_sound_start(&song.sound);
+            logger("'r' key pressed, audio restarted from 00:00:00, or pcm frame 0.");
+        }
+    
+        else if (keys[VK_RIGHT].current) {
+            ma_uint64 current_frame;
+            ma_sound_get_cursor_in_pcm_frames(&song.sound, &current_frame);
+            if ((current_frame + song.skipframes) < song.totalframes) {
+                    ma_sound_seek_to_pcm_frame(&song.sound, (current_frame + song.skipframes));
+                    logger("right arrow key pressed, seeked 5 seconds forward into audio.");
+                }
+                else {
+                    ma_sound_seek_to_pcm_frame(&song.sound, song.totalframes - song.sample_rate);
+                    logger("right arrow key pressed, but skipping 5 seconds forward went out of audio scope, resorted to playing the last second of audio.");
+                }
+            rest(230);
+        }
+
+        else if (keys[VK_LEFT].current) {
+            ma_uint64 current_frame;
+            ma_sound_get_cursor_in_pcm_frames(&song.sound, &current_frame);
+            if (current_frame > song.skipframes) {
+                    ma_sound_seek_to_pcm_frame(&song.sound, (current_frame - song.skipframes));
+                    logger("left arrow key pressed, seeked 5 seconds backward into audio.");
+                }
+                else {
+                    ma_sound_seek_to_pcm_frame(&song.sound, 0);
+                    logger("left arrow key pressed, but skipping 5 seconds backward went out of audio scope, resorted to playing from audio start.");
+                }
+            rest(230);
+            }
+
+        rest(20);
+    }
 }
 
 int main() {
@@ -60,78 +149,20 @@ int main() {
     
     ma_sound_get_data_format(&song.sound, &song.format, &song.channels, &song.sample_rate, NULL, 0);
     ma_sound_get_length_in_pcm_frames(&song.sound, &song.totalframes);
-    ma_uint64 skip_frames = 5 * song.sample_rate;
+    song.skipframes = 5 * song.sample_rate;
     logger("sound/song/audio info scanned and set.");
     
-
     ma_sound_start(&song.sound);
     logger("sound/song/audio playing.");
     
+    std::thread input_handler_thread(song_input_handler, std::ref(song));
+    logger("input collection thread started.");
     while (song_process_running) {
-        
-        int userinput = _getch();
-        if (userinput == ' ') {if (ma_sound_is_playing(&song.sound) == MA_TRUE) {ma_sound_stop(&song.sound); logger("spacebar pressed, audio playback paused.");} else {ma_sound_start(&song.sound); logger("spacebar pressed, audio playing");}}
-        
-        else if (userinput == 'r' || userinput == 'p') {
-            ma_sound_stop(&song.sound);
-            ma_sound_seek_to_pcm_frame(&song.sound, 0); 
-            ma_sound_start(&song.sound); 
-            if (userinput == 'r') {
-                logger("'r' key pressed, audio restarted from 00:00:00.");
-            } 
-            else {
-                logger("'p' key pressed, audio restarted from 00:00:00, or playing previous song.");
-            }
-        }
-        
-        else if (userinput == 27) {
-            ma_sound_stop(&song.sound); 
-            song_process_running = false; 
-            logger("'esc' key pressed, proceeding to exit.");}
-
-        else if (userinput == 224 || userinput == 0) {
-            
-            int secondinput = _getch();
-            ma_uint64 current_frame;
-            ma_sound_get_cursor_in_pcm_frames(&song.sound, &current_frame);
-            
-            if (secondinput == 77) {
-                if ((current_frame + skip_frames) < song.totalframes) {
-                    ma_sound_seek_to_pcm_frame(&song.sound, (current_frame + skip_frames));
-                    logger("right arrow key pressed, seeked 5 seconds forward into audio.");
-                }
-                else {
-                    ma_sound_seek_to_pcm_frame(&song.sound, song.totalframes - song.sample_rate);
-                    logger("right arrow key pressed, but skipping 5 seconds forward went out of audio scope, resorted to playing the last second of audio.");
-                }
-            }
-            
-            else if (secondinput == 75) {
-                if (current_frame > skip_frames) {
-                    ma_sound_seek_to_pcm_frame(&song.sound, (current_frame - skip_frames));
-                    logger("left arrow key pressed, seeked 5 seconds backward into audio.");
-                }
-                else {
-                    ma_sound_seek_to_pcm_frame(&song.sound, 0);
-                    logger("left arrow key pressed, but skipping 5 seconds backward went out of audio scope, resorted to playing from the start.");
-                }
-            }
-
-            else if (secondinput == 80 || secondinput == 72) {
-                
-                if (secondinput == 72) {
-                    //add later - next song
-                    logger("up arrow key pressed, skipped to next audio file in queue.");
-                }
-                
-                else {
-                    //add later - previous song or skip_frames
-                    logger("down arrow key pressed, audio restarted from 00:00:00, or playing previous song.");
-                }
-            }
-        }
+        rest(1000);
     }
     
+    input_handler_thread.join();
+    logger("input collection thread successfully closed.");
     ma_sound_uninit(&song.sound); ma_engine_uninit(&engine);
     logger("cleanup successful, exit.");
     
